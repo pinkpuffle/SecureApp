5936044c8361f5202e1d4e58f198ad112b093b96
purplepuffle <francescajacques64@gmail.com>
2023-12-01 19:05:02 +0000

    dob

--- index.php
+++ index.php
@@ -13,23 +13,19 @@ function validationForm(){
 
 <form name="register" action="submitForm.php" onsubmit="return validateForm()" method="post">
     
+    <input type="text" id="username" name="username" placeholder="Username"><br>
+
+    <input type="password" id="password" name="password" placeholder="Password"><br>
+
+    <input type="password" id="repeatPassword" name="repeatPassword" placeholder="Repeat password"><br>
+
+    <input type="email" id="email" name="email" placeholder="Email"><br>
+
     <input type="text" id="firstName" name="firstName" placeholder="First name">
-    <input type="text" id="lastName" name="lastName" placeholder="Last name">
-    <br>
+    <input type="text" id="lastName" name="lastName" placeholder="Last name"><br>
 
-    <input type="text" id="username" name="username" placeholder="Username">
-    <br>
-
-    <input type="password" id="password" name="password" placeholder="Password">
-    <br>
-
-    <input type="password" id="repeatPassword" name="repeatPassword" placeholder="Repeat password">
-    <br>
-
-    <input type="email" id="email" name="email" placeholder="Email">
-    <br>
-
-
+    <label for="dateOfBirth">Date of Birth</label><br>
+    <input type="date" id="dateOfBirth" name="dateOfBirth"><br>
 
 </form>
 
