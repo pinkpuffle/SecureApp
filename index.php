d18c33aa968c02340eaebe65b3170bab0bbc940b
purplepuffle <francescajacques64@gmail.com>
2023-12-01 18:43:54 +0000

    form structure

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <html>
+<head>
+<script>
+function validationForm(){
+    
+}
+
+</script>
+
+</head>
+
 <body>
+
+<form name="register" action="submitForm.php" onsubmit="return validateForm()" method="post">
+
+</form>
+
+
 
 </body>
 </html>
