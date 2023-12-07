17b5955f6edd7cf71de895a7e183c41e1883df9f
purplepuffle <francescajacques64@gmail.com>
2023-12-07 10:23:24 +0000

    db

--- submitForm.php
+++ submitForm.php
@@ -28,20 +28,57 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-function insertUser($username, $password){
+function insertUser($conn, $username, $password){
+    $stmt = $conn->prepare("INSERT INTO users (username, password)
+    VALUES (?, ?)");
+    $stmt->bind_param("ss",$username,$password);
 
+    if($stmt->execute() === TRUE){
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
-function insertUserDetails($firstName, $lastName, $email, $phoneNumber, $dateofBirth, $gender){
-
+function getUserID($conn, $username){
+    $stmt = $conn->prepare("SELECT userID FROM users WHERE username = ?");
+	$stmt->bind_param("s",$username);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$row = $result->fetch_assoc();
+    return $row["userID"];
 }
 
-function insertAddress($addressNumber, $addressL1, $addressL2, $town, $county, $postcode, $country){
+function insertUserDetails($conn, $userID, $firstName, $lastName, $email, $phoneNumber, $dateofBirth, $gender){
+    $stmt = $conn->prepare("INSERT INTO userdetails (userID, firstName, lastName, email, phoneNumber, dateOfBirth, gender)
+    VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("issssss",$userID, $firstName, $lastName, $lastName, $email, $phoneNumber, $dateOfBirth, $gender);
 
+    if($stmt->execute() === TRUE){;
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
-function insertPet(){
+function insertAddress($conn, $userID, $addressNumber, $addressL1, $addressL2, $town, $county, $postcode, $country){
+    $stmt = $conn->prepare("INSERT INTO addresses (userID, addressNumber, lineOne, lineTwo, town, county, postcode, country)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("isssssss",$userID, $addressNumber, $addressL1, $addressL2, $town, $county, $postcode, $country);
 
+    if($stmt->execute() === TRUE){;
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+function insertPet($conn, $userID, $petName, $petType, $petAge){
+    $stmt = $conn->prepare("INSERT INTO userdetails (userID, name, type, age)
+    VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("issi",$userID, $petName, $petType, $petAge);
+
+    if($stmt->execute() === TRUE){;
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
 ?>
